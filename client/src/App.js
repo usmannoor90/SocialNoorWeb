1e23822e55ef28b969b5e5b9ae98c157e3d4f3b0
MinhaalAzhar94 <m.usmannoor90@gmail.com>
2023-11-29 19:59:32 +0500

    dd

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
